7f3d9c79ac0d1f13afb3df71e53c5c410342ba1a
gi0ste <34270727+gi0ste@users.noreply.github.com>
2020-02-28 18:12:21 +0100

    Create model.js

---
+++ model.js
@@ -0,0 +1,34 @@
+{
+    "interactionModel": {
+        "languageModel": {
+            "invocationName": "pronto aiuto",
+            "intents": [
+                {
+                    "name": "AMAZON.CancelIntent",
+                    "samples": []
+                },
+                {
+                    "name": "AMAZON.HelpIntent",
+                    "samples": []
+                },
+                {
+                    "name": "AMAZON.StopIntent",
+                    "samples": []
+                },
+                {
+                    "name": "AMAZON.NavigateHomeIntent",
+                    "samples": []
+                },
+                {
+                    "name": "WaitingList",
+                    "slots": [],
+                    "samples": [
+                        " quanti pazienti",
+                        "quanti pazienti ci sono in attesa"
+                    ]
+                }
+            ],
+            "types": []
+        }
+    }
+}
