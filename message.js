4b97dd4244c0a4393318091de125dcdb47ecccee
gi0ste <34270727+gi0ste@users.noreply.github.com>
2020-02-28 12:22:08 +0100

    Create message.js

---
+++ message.js
@@ -0,0 +1,21 @@
+const welcomeMessage    = 'Ciao, indovina a quale animale sto pensando'
+const helpMessage       = 'Sto pensando ad un animale. Prova ad indovinarlo dicendo il nome. Ad esempio cane'
+const correctAnswer     = 'Hai indovinato. Stavo proprio pensando a '
+const playAgain         = 'Vuoi giocare ancora?'
+const exitMessage       = 'Grazie per aver giocato con noi. A presto'
+const repromtMessage    = 'Dai prova ancora'
+const wrongAnswer       = 'non è '
+const speechConsCorrect = ['Complimenti', 'Hurrah', 'Evviva', 'Fammi pensare', 'Siii'];
+const speechConsWrong   = ['Fammi pensare', 'Accidenti', 'Mannaggia', 'No'] 
+
+module.exports = {
+    welcomeMessage,
+    helpMessage,
+    correctAnswer,
+    playAgain,
+    exitMessage,
+    repromtMessage,
+    wrongAnswer,
+    speechConsCorrect,
+    speechConsWrong,
+}
